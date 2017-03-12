47823e53bfaf029782b8ae70d453aeb6d3d82dc3
進興 李 <vulcan@miniasp.com>
2017-03-12 15:22:32 +0800

    0312練習情境頁面導航

---
+++ XFDDeeply/XFDDeeply/XFDDeeply/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XFDDeeply.ViewModels
+{
+
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        #region Repositories (遠端或本地資料存取)
+
+        #endregion
+
+        #region ViewModel Property (用於在 View 中作為綁定之用)
+
+        #endregion
+
+        #region Field 欄位
+
+        public DelegateCommand 深度導航Command { get; set; }
+
+        public DelegateCommand SwitchPage1Command { get; set; }
+
+        private readonly INavigationService _navigationService;
+        #endregion
+
+        #region Constructor 建構式
+        public MainPageViewModel(INavigationService navigationService)
+        {
+
+            #region 相依性服務注入的物件
+
+            _navigationService = navigationService;
+            #endregion
+
+            #region 頁面中綁定的命令
+            SwitchPage1Command = new DelegateCommand(() =>
+            {
+                _navigationService.NavigateAsync("Page1Page");
+            });
+            深度導航Command = new DelegateCommand(() =>
+            {
+                _navigationService.NavigateAsync("Page1Page/Page2Page/Page3Page");
+            });
+            #endregion
+
+            #region 事件聚合器訂閱
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Navigation Events (頁面導航事件)
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            await ViewModelInit();
+        }
+        #endregion
+
+        #region 設計時期或者執行時期的ViewModel初始化
+        #endregion
+
+        #region 相關事件
+        #endregion
+
+        #region 相關的Command定義
+        #endregion
+
+        #region 其他方法
+
+        /// <summary>
+        /// ViewModel 資料初始化
+        /// </summary>
+        /// <returns></returns>
+        private async Task ViewModelInit()
+        {
+            await Task.Delay(100);
+        }
+        #endregion
+
+    }
+
+}
---
+++ XFDDeeply/XFDDeeply/XFDDeeply/Views/NaviPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFDDeeply.Views
+{
+    public partial class NaviPage : NavigationPage
+    {
+        public NaviPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
