d699aa4cebdb3fcf90c1e8a60f487eb6690253a9
進興 李 <vulcan@miniasp.com>
2017-03-18 17:11:38 +0800

    ListView控制項綁定ViewModel的資料

---
+++ XFListView1/XFListView1/XFListView1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace XFListView1.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region 學生s
+        private ObservableCollection<學生> _學生s = new ObservableCollection<學生>();
+        /// <summary>
+        /// 學生s
+        /// </summary>
+        public ObservableCollection<學生> 學生s
+        {
+            get { return _學生s; }
+            set { SetProperty(ref _學生s, value); }
+        }
+        #endregion
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+
+            for (int i = 0; i < 100; i++)
+            {
+                學生s.Add(new 學生
+                {
+                    姓名 = $"學生 {i}",
+                });
+            }
+        }
+    }
+}
---
+++ XFListView1/XFListView1/XFListView1/學生.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFListView1
+{
+    public class 學生
+    {
+        public string 姓名 { get; set; }
+    }
+}
