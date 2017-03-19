05523c768d37d398ea7f3ce2b1edab71c98d6dc8
進興 李 <vulcan@miniasp.com>
2017-03-19 16:17:48 +0800

    數值轉換器 Value Converter

---
+++ XFValueConverter/XFValueConverter/XFValueConverter/StringToColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFValueConverter
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           if(value is string)
+            {
+                var foo = value as string;
+                if (foo == "1")
+                {
+                    return Color.Green;
+                } else if (foo == "2")
+                {
+                    return Color.Red;
+                }
+                else if (foo == "3")
+                {
+                    return Color.Blue;
+                }
+            }
+            return Color.Accent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ XFValueConverter/XFValueConverter/XFValueConverter/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFValueConverter.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region InputColor
+        private string _InputColor;
+        /// <summary>
+        /// InputColor
+        /// </summary>
+        public string InputColor
+        {
+            get { return this._InputColor; }
+            set { this.SetProperty(ref this._InputColor, value); }
+        }
+        #endregion
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
