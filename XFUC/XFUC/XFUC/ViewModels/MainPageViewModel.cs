f2e63b1ddc8e453ada4f9f656fd91216dcc2c3fc
李進興 Vulcan <vulcan@miniasp.com>
2017-03-25 13:53:54 +0800

    使用者控制項 User Control

---
+++ XFUC/XFUC/XFUC/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFUC.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region Name
+        private EntryCheckViewModel _Name;
+        /// <summary>
+        /// 這個屬性，將會用於綁定到 姓名 欄位輸入
+        /// </summary>
+        public EntryCheckViewModel Name
+        {
+            get { return this._Name; }
+            set { this.SetProperty(ref this._Name, value); }
+        }
+        #endregion
+
+        public MainPageViewModel()
+        {
+            Name = new EntryCheckViewModel();
+            Name.更新文字輸入盒的浮水印文字設定("Name");
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ XFUC/XFUC/XFUC/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFUC.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
